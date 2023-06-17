783b910df1ad8511fee5fb746197c934b9b7b6d5
Mariam Babutsidze <mariambabutsidze@gmail.com>
2023-06-17 15:54:24 +0400

    Initial Commit

---
+++ FurFamilyFinder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FurFamilyFinder
+//
+//  Created by Mariam Babutsidze on 17.06.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FurFamilyFinder/FurFamilyFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  FurFamilyFinderApp.swift
+//  FurFamilyFinder
+//
+//  Created by Mariam Babutsidze on 17.06.23.
+//
+
+import SwiftUI
+
+@main
+struct FurFamilyFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
